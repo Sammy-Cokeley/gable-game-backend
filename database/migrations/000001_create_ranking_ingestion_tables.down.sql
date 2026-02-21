6cef8f52f07df4d735fcdc24eb345de72c0e4901
Sammy Cokeley <90985489+Sammy-Cokeley@users.noreply.github.com>
2026-02-21 08:59:03 -0600

    Add ranking ingestion schema and CSV snapshot importer

---
+++ database/migrations/000001_create_ranking_ingestion_tables.down.sql
@@ -0,0 +1,6 @@
+DROP INDEX IF EXISTS idx_ranking_entry_snapshot;
+DROP INDEX IF EXISTS idx_ranking_snapshot_lookup;
+
+DROP TABLE IF EXISTS ranking_entry;
+DROP TABLE IF EXISTS ranking_snapshot;
+DROP TABLE IF EXISTS ranking_source;
